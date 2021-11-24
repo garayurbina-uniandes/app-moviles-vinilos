027c998ff3aef9e583fef56bce888d98b13a37eb
CAMP <ca.manzo973@uniandes.edu.co>
2021-11-24 17:26:01 -0500

    Print Tracks in Album Detail

---
+++ app/src/main/java/com/vinylsMobile/vinylsApplication/data/model/TracksResponse.kt
@@ -0,0 +1,14 @@
+package com.vinylsMobile.vinylsApplication.data.model
+
+import com.google.gson.annotations.SerializedName
+
+class TracksResponse {
+    @SerializedName("id")
+    var id: Number = 0
+
+    @SerializedName("name")
+    var name: String? = null
+
+    @SerializedName("duration")
+    var duration: String? = null
+}
